1d5cd4fbdece58382e66d43f010dfffd08452919
Nana Kwame Biney <102677477+10949743@users.noreply.github.com>
2022-08-08 10:59:33 +0000

    Add files via upload

---
+++ DCIT 104 assignment Nana Kwame Biney(10949743).cpp
@@ -0,0 +1,40 @@
+// Nana Kwame Biney (10949743)
+//program for producing the sum and average of prime numbers less than F
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int F)
+{
+    //condition for F to work
+    if(F < 2)
+        return false;
+
+    //checking for prime number
+    for(int a = 2; a < F; a++)
+        if(F % == 0)
+            return false;
+
+    return true;
+}
+
+//printing the prime numbers less than F
+void printprime(int F)
+{
+    cout <<"The prime numbers are:";
+    for(int a = 2; a  < F; a++)
+        if(isPrime(j))
+            cout << a <<"";
+
+}
+
+//sum of prime numbers less than F
+int main()
+{
+    int (sum = 0)
+    sum += a
+    a++
+}
+
+//average of prime numbers
+float average = sum/a
